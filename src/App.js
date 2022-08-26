d80ef98f269d5c0ac007e36615a80bd00d6f4244
tanhoang0710 <tanhoang0710@gmail.com>
2022-08-26 11:25:54 +0700

    Design trello frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,250 @@
-import './App.css';
+import './App.scss';
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello world</h1>
+		<div className="trello-master">
+			<nav className="navbar app">App Bar</nav>
+			<nav className="navbar board">Board Bar</nav>
+			<div className="board-columns">
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className="column">
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src="https://avatars.githubusercontent.com/u/82456133?v=4"
+								alt=""
+							/>
+							Title: tanhun
+						</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+						<li>Add what you'd like to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+			</div>
 		</div>
 	);
 }
